60a4969e305d19e82f645cc532e031d1efaf2a59
saleha <saleha472@gmail.com>
2016-06-02 12:27:12 +0530

    projects committed for tests and webapi

---
+++ WebAPIAssignments.Tests/Controllers/ContactControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebAPIAssignments.Interfaces;
+using WebAPIAssignments.Repository;
+using WebAPIAssignments.ViewModel;
+using WebAPIAssignments.Models;
+
+namespace WebAPIAssignments.Tests.Controllers
+{
+    [TestClass]
+    public class ContactControllerTest
+    {
+        private Mock<IContactRepository> _Contact;
+        public ContactControllerTest()
+        {
+            _Contact = new Mock<IContactRepository>();
+        }
+         
+        [TestMethod]
+        public void ContactDetailsTest()
+        {
+            ContactViewModel List = new ContactViewModel()
+            {
+                NewContact = new ContactModel()
+            };
+            //List.Details=_Contact.E
+            
+        }
+    }
+}
